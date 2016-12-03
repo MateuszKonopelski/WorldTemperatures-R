a713547a9283eac5d97c8d89f9ebacf5c098fe17
Matt <panpluto@gmail.com>
2016-12-03 19:11:01 +0000

    Update R_Export_toSQL.R

--- CODE/MineDevelopment/R_Export_toSQL.R
+++ CODE/MineDevelopment/R_Export_toSQL.R
@@ -6,15 +6,30 @@
 require(RSQLite)
 require(sqldf)
 
+#set directory
 setwd("~/Documents/PROJECTS/Temperature_Data/Data/Test_ghcnd_all/100")
 
+#check if db exists, fi yes, delete
+if (file.exists("gcnd_all_100.sqlite") == TRUE) {file.remove("gcnd_all_100.sqlite") == TRUE}
+
+#Create new SQLite db
 db <- dbConnect(SQLite(),dbname = "gcnd_all_100.sqlite")
 
-#dbRemoveTable(db, "ghcn")
+#check if ghcn in db exists, if yes, delete.
+if (dbExistsTable(db, name = "ghcn") == TRUE) {dbRemoveTable(db, "ghcn")}
+
+#write into db, table ghcn from Data table
 dbWriteTable(conn = db, name = "ghcn", value = Data, row.names = FALSE)
 
-TimeEnd <- Sys.time()
 
+#check time of execution
+TimeEnd <- Sys.time()
 Method2_TimeExec <- TimeEnd - TimeStart
-rm("TimeStart","TimeEnd")
-Method2_TimeExec
+
+if (file.exists("ExecTime_gcnd_all_100.txt") == TRUE) {file.remove("ExecTime_gcnd_all_100.txt") == TRUE}
+write(Method2_TimeExec, file = "ExecTime_gcnd_all_100.txt")
+
+
+
+#Clear Global Environment
+rm(list=ls())
